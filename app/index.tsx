07f27f19f5ef914636b696c003ea787bfa47fa82
Aditya Narayan <adityanarayan29j@gmail.com>
2024-07-17 01:39:23 +0400

    feat: Initial const setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
+import {useState} from "react";
+import { Text, ScrollView,SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
 
-import { Text, View } from "react-native";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from "../components";
 
 export default function Index() {
+  const Router = useRouter();
   return (
-    <View>
+    <SafeAreaView>
       <Text>Home</Text>
-    </View>
+    </SafeAreaView>
   );
 }
